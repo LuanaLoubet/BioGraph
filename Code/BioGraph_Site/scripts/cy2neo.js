d23eaac65f2e8d0b97274d413d77b533fd12edfa
hercules <hercules.cardoso.silva1@gmail.com>
2018-04-08 20:12:53 -0400

    adicionando menu de request de nós por label

--- Code/BioGraph_Site/scripts/cy2neo.js
+++ Code/BioGraph_Site/scripts/cy2neo.js
@@ -21,11 +21,14 @@ function Cy2Neo(config, graphId, sourceId, execId, urlSource) {
 	}
 	config = initAlchemyConfig();
 	var neo = new Neo(urlSource);
-    var editor = createEditor();
-	/*$("#"+execId).click(function(evt) {
+    //var editor = createEditor();
+	$("#"+execId).click(function(evt) {
 		try {
+
 			evt.preventDefault();
-			var query = editor.getValue();
+			var e =  document.getElementById("SelectLabels");
+           var value =  e.options[e.selectedIndex].value;
+			var query =   "MATCH (n:"+value+") RETURN n LIMIT 50";
 			console.log("Executing Query",query);
 			neo.executeQuery(query,{},function(err,res) {
 				res = res || {}
@@ -44,7 +47,7 @@ function Cy2Neo(config, graphId, sourceId, execId, urlSource) {
 		}
 		return false;
 	});
-*/
+
 
 
 
@@ -70,13 +73,37 @@ function getQueryElement(consulta,rGraph,rLabels) {
             selectList.id = "SelectLabels";
             myDiv.appendChild(selectList);
             var i;
+            var list= Array();
             for(i=0;i<rLabels.length;i++)
 			{
-                var option = document.createElement("option");
-                option.value = rLabels[i]["labelS(n)"][1];
-                option.text = rLabels[i]["labelS(n)"][1];
-                selectList.appendChild(option);
+                //var option = document.createElement("option");
+                if(rLabels[i]["labelS(n)"].length==2) {
+                	if($.inArray(rLabels[i]["labelS(n)"][1],list)<0)
+					{
+							list.push(rLabels[i]["labelS(n)"][1]);
+					}
+					if($.inArray(rLabels[i]["labelS(n)"][0],list)<0)
+                    {
+                        list.push(rLabels[i]["labelS(n)"][0]);
+                    }
+                		//option.value = rLabels[i]["labelS(n)"][1];
+                    //option.text = rLabels[i]["labelS(n)"][1];
+                    //selectList.appendChild(option);
+                }
+                if(rLabels[i]["labelS(n)"].length==1) {
+                    if($.inArray(rLabels[i]["labelS(n)"][0],list)<0)
+                    {
+                        list.push(rLabels[i]["labelS(n)"][0]);
+                    }
+                }
 			}
+			for(i=0;i<list.length;i++)
+			{
+                var option = document.createElement("option");
+                option.value = list[i];
+                option.text = list[i];
+                selectList.appendChild(option);
+            }
 
 
             config.nodeTypes = {type: labels};
