d0bea8de15598e49d1ade84562a03c1580e16eed
hercules <hercules.cardoso.silva1@gmail.com>
2018-05-02 19:59:10 -0400

    adicionando grafo

--- Code/BioGraph_Site/scripts/cy2neo.js
+++ Code/BioGraph_Site/scripts/cy2neo.js
@@ -55,6 +55,63 @@ function Cy2Neo(config, graphId, sourceId, execId, urlSource) {
 }
 
 
+
+function Cy2NeoRelacionamento(config, graphId, sourceId, execId, urlSource) {
+    function createEditor() {
+        return CodeMirror.fromTextArea(document.getElementById(sourceId), {
+            parserfile: ["codemirror-cypher.js"],
+            path: "scripts",
+            stylesheet: "styles/codemirror-neo.css",
+            autoMatchParens: true,
+            lineNumbers: true,
+            enterMode: "keep",
+            value: "some value"
+        });
+    }
+    function initAlchemyConfig() {
+        config.divSelector="#"+graphId;
+        config.dataSource={nodes:[],edges:[]};
+        config.forceLocked = false;
+        config.alpha = 0.8;
+        config.edgeTypes = "caption";
+        alchemy.begin(config)
+        return config;
+    }
+    config = initAlchemyConfig();
+    var neo = new Neo(urlSource);
+    //var editor = createEditor();
+    $("#"+execId).click(function(evt) {
+        try {
+
+            evt.preventDefault();
+            var e =  document.getElementById("SelectLabels");
+            var value =  e.options[e.selectedIndex].value;
+            var query = "MATCH (n:"+value+")-[r]-() RETURN n,r LIMIT 25";
+            console.log("Executing Query",query);
+            neo.executeQuery(query,{},function(err,res) {
+                res = res || {}
+                var graph=res.graph;
+                var labels = res.labels;
+                config.nodeTypes = {type: labels};
+                if (err) {
+                    alchemy.conf.warningMessage=JSON.stringify(err);
+                    alchemy.startGraph(null)
+                } else {
+                    alchemy.startGraph(graph);
+                }
+            });
+        } catch(e) {
+            console.log(e);
+        }
+        return false;
+    });
+
+
+
+
+
+}
+
 function getQueryElement(consulta,rGraph,rLabels) {
     try {
     	var neo = new Neo("http://pandora.lis.ic.unicamp.br:7474/db/data/");
